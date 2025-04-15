ee86a19b93e0c856323887176f6df30596b080ee
sletuffe <sylvain@letuffe.org>
2025-04-15 14:47:29 +0200

    Amélioration du script de renommage de tables

--- maintenance/from-temporary-tables-during-import-to-production.sh
+++ maintenance/from-temporary-tables-during-import-to-production.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # This script will help you after import in temporary tables with a temporary prefix used in osm2pgsql
-# to delete old tales and remove them by new one, reducing downtime
+# to delete old tables and remove them by new one, reducing downtime
 # Of course, you need twice the disk space to use that !
 
-. ../config.sh
+. $(dirname $0)/../config.sh
 
 if [ "$1" == "" ] || [ "$2" == "" ] || [ "$1" == "-h" ] ; then
-  echo "Usage : ./from-temporary-tables-during-import-to-production.sh <temporary_prefix> <production_prefix>"
-  echo "Example : ./from-temporary-tables-during-import-to-production.sh  planet_osm_temporary  planet_osm"
+  echo "  Usage : ./from-temporary-tables-during-import-to-production.sh <temporary_prefix> <production_prefix>"
+  echo "  Example : ./from-temporary-tables-during-import-to-production.sh  planet_osm_temporary  planet_osm"
+  exit 0
 fi
 
 
-for x in line nodes point polygone rels roads ways ; do 
-  echo "drop table $2_$x;" | psql $base_osm
+for x in line nodes point polygon rels roads ways ; do 
+  echo "drop table $2_$x cascade;" | psql $base_osm
   echo "ALTER TABLE $1_$x RENAME TO $2_$x;" | psql $base_osm
 done
 
