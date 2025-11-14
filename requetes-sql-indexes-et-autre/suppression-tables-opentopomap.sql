df350620342656a534de92c74e53b81ee10175be
sletuffe <sylvain@letuffe.org>
2025-11-14 18:01:52 +0100

    New script to drop dedicated opentopomap tables

---
+++ requetes-sql-indexes-et-autre/suppression-tables-opentopomap.sql
@@ -0,0 +1,10 @@
+-- remember that osm2pgsql do that with -c
+drop table borders;
+drop table cities;
+drop table lakelabels;
+drop table landuse;
+drop table naturalarealabels;
+drop table railways;
+drop table roads;
+drop table water;
+
