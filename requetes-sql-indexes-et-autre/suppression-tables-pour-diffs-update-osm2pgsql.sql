d18451ce2cc9441d393fb1c6e30fd4b2f47f01a1
sletuffe <sylvain@letuffe.org>
2026-02-11 13:03:23 +0100

    wrings tables to drop when no needs for diff update

---
+++ requetes-sql-indexes-et-autre/suppression-tables-pour-diffs-update-osm2pgsql.sql
@@ -0,0 +1,4 @@
+-- osm2pgsql do that with --drop but in case you forgot to use --drop, those are the 3 tables to drop
+drop table planet_osm_ways;
+drop table planet_osm_nodes;
+drop table planet_osm_rels;
